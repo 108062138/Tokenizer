a76512d63bb0b1f6d18fe1ed23da997efdbda5a2
108062138 <poyuwuwu17@gmail.com>
2022-04-12 15:48:17 +0800

    rem this before claer everything out

---
+++ testcases/comment2Test.c
@@ -0,0 +1,8 @@
+//tmp dsfd fdsffdfdsf /**/
+/*fdsdff /*******++
+ * +_)(*&^%$#@!~+_)(*&^%}"?><MNBVCXZ
+ */
+//tmp dsfds //ttas setds //////////// /* dfd 
+int flaot fdstfdfd ;
+loatFl float int int int intintintin ttt//*********
+fds fds /**//**//
---
+++ testcases/stringTest.c
@@ -0,0 +1,23 @@
+'a'	"dddd	  	123"
+"123	"
+"int a" "/**/" //david~~~~ heheahha~~~ */
+/* \n\a\b\c\t\ff"~~~~_AAAA"
+ * auto text
+ * alone
+ * together 
+ * alone
+ * ~~~~
+ */  		*int*string*int;/*dddd*/*+++++++----==++-=-="~\'\"\"\"\"\""pins			//
+"tmp \" hah\"\" \t \a he	he\'" int a	;
+"test~~..." for	int=1(;)+1++'@'	'\n'	's' "a123v df  \n \a \b\c\"\'\"\"\"    /**//*** same*/__"
+.dfds ..,,,..dfd 1.,,.,4545 "dfds sdkfjsd                                   " "dfd 123 \n \n\\\\\\\n"
+"dsfdf tmp filone 123@@@@@ ********\*\*\\\\*\___~~~~~>"":{()#$EWWE      E ....EJ}        "
+
+
+int aa;; dfd sstl;  "a12321 0-9-985443-05>:"
+{}}}|||{{{{
+    "~!!!@#$"
+    %5555%%%++++------00
+    0%%&*(
+    )+}{:
+    "<>~~<>?"}}}}
